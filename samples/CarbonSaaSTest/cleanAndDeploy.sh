194966b6bb2ca16fd0e21c2efe72f0f34c609d4f
Amila Shamika Ariyawansa <shamikaa@shamika-LP.(none)>
2014-03-17 15:26:48 +0530

    Removed unnessasry files from Saas App

--- samples/CarbonSaaSTest/cleanAndDeploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-rm -r /home/shamikaa/wso2/marketing/2373/wso2as-5.2.1/repository/deployment/server/webapps/SaasTest*
-cp target/SaasTest-1.0.0.war /home/shamikaa/wso2/marketing/2373/wso2as-5.2.1/repository/deployment/server/webapps
